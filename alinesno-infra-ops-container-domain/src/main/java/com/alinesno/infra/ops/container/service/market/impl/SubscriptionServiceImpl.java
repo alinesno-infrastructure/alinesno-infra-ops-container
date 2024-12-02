9892125e50a4344a6844176f558b364d510fa63b
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:18 +0800

    更新:alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/service/market/impl/SubscriptionServiceImpl.java

--- alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/service/market/impl/SubscriptionServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.ops.container.service.market.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.ops.container.entity.market.SubscriptionEntity;
-import com.alinesno.infra.ops.container.mapper.market.SubscriptionMapper;
-import com.alinesno.infra.ops.container.service.market.ISubscriptionService;
-
-public class SubscriptionServiceImpl extends IBaseServiceImpl<SubscriptionEntity, SubscriptionMapper> implements ISubscriptionService {
-}
