75693eca1982b6fe23345640cd691b4eed78b868
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:18 +0800

    更新:alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/market/CommentMapper.java

--- alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/market/CommentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.ops.container.mapper.market;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.ops.container.entity.market.CommentEntity;
-
-public interface CommentMapper extends IBaseMapper<CommentEntity> {
-}
