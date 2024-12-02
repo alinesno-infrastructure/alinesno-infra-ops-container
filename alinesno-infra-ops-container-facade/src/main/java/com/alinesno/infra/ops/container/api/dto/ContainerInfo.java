f97f32f688c09845d6d1a7e198b69e372f8978fd
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    refactor(ContainerInfo): 使用 Lombok 注解简化 ContainerInfo 类的getter、setter和toString方法。

--- alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/api/dto/ContainerInfo.java
+++ alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/api/dto/ContainerInfo.java
@@ -1,43 +1,10 @@
 package com.alinesno.infra.ops.container.api.dto;
 
+import lombok.Data;
+
+@Data
 public class ContainerInfo {
-        private String name;
-        private String description;
-        private String icon;
-
-        // getter and setter methods
-
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public String getIcon() {
-            return icon;
-        }
-
-        public void setIcon(String icon) {
-            this.icon = icon;
-        }
-
-        @Override
-        public String toString() {
-            return "ContainerInfo{" +
-                    "name='" + name + '\'' +
-                    ", description='" + description + '\'' +
-                    ", icon='" + icon + '\'' +
-                    '}';
-        }
-    }
+    private String name;
+    private String description;
+    private String icon;
+}
