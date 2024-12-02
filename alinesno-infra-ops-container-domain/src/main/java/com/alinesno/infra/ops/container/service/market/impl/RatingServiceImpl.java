dfec07213ef71a62b9555b916e128b679a8222aa
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:17 +0800

    更新:alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/service/market/impl/RatingServiceImpl.java

--- alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/service/market/impl/RatingServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.ops.container.service.market.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.ops.container.entity.market.RatingEntity;
-import com.alinesno.infra.ops.container.mapper.market.RatingMapper;
-import com.alinesno.infra.ops.container.service.market.IRatingService;
-
-public class RatingServiceImpl extends IBaseServiceImpl<RatingEntity, RatingMapper> implements IRatingService {
-}
