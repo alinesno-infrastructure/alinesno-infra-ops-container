8fe5eb5f6893c2cf51dbf8714ec1bf862829139e
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    refactor(ContainerDetails.java): 使用 Lombok 注解简化代码

--- alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/api/dto/ContainerDetails.java
+++ alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/api/dto/ContainerDetails.java
@@ -1,33 +1,9 @@
 package com.alinesno.infra.ops.container.api.dto;
 
+import lombok.Data;
+
+@Data
 public class ContainerDetails {
-        private String namespace;
-        private String initRun;
-
-        // getter and setter methods
-
-
-        public String getNamespace() {
-            return namespace;
-        }
-
-        public void setNamespace(String namespace) {
-            this.namespace = namespace;
-        }
-
-        public String getInitRun() {
-            return initRun;
-        }
-
-        public void setInitRun(String initRun) {
-            this.initRun = initRun;
-        }
-
-        @Override
-        public String toString() {
-            return "ContainerDetails{" +
-                    "namespace='" + namespace + '\'' +
-                    ", initRun='" + initRun + '\'' +
-                    '}';
-        }
-    }
+    private String namespace;
+    private String initRun;
+}
