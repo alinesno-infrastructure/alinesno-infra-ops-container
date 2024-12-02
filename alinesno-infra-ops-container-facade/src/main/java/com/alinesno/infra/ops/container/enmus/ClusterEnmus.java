e0068d3cbd93b757c48b8bb7cf305c7d8f5323a0
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    优化:精简 ClusterEnmus 枚举类代码并添加 Lombok 注解

--- alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/enmus/ClusterEnmus.java
+++ alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/enmus/ClusterEnmus.java
@@ -1,45 +1,22 @@
 package com.alinesno.infra.ops.container.enmus;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * 是否当前集群枚举
  *
  * @author luoxiaodong
  * @version 1.0.0
  */
+@Getter
+@RequiredArgsConstructor
 public enum ClusterEnmus {
 
 	CURRENT("1", "当前集群"), NO_CURRENT("0", "非当前集群");
 
-	private String value;
-	private String label;
+	private final String value;
+	private final String label;
 
-	/**
-	 * 构造方法
-	 * 
-	 * @param value 枚举值
-	 * @param label 枚举标签
-	 */
-	ClusterEnmus(String value, String label) {
-		this.value = value;
-		this.label = label;
-	}
-
-	/**
-	 * 获取枚举值
-	 * 
-	 * @return 枚举值
-	 */
-	public String getValue() {
-		return value;
-	}
-
-	/**
-	 * 获取枚举标签
-	 * 
-	 * @return 枚举标签
-	 */
-	public String getLabel() {
-		return label;
-	}
 
 }
