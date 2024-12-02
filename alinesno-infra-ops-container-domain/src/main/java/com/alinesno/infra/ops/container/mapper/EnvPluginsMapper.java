7e8ce47b5ae4f1fc8ada96bc234b0d22453d45cb
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    feat:新增环境插件映射接口 `EnvPluginsMapper`。

---
+++ alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/EnvPluginsMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.ops.container.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.ops.container.entity.EnvPluginsEntity;
+
+/**
+ * 环境插件 Mapper接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface EnvPluginsMapper extends IBaseMapper<EnvPluginsEntity> {
+}
