ae3150ba70e66cfc5289fcff0d57c70f680dc3f1
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    refactor: 使用 Lombok 注解简化 ContainerConfigDto 类的 getter 和 setter 方法。

--- alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/api/dto/ContainerConfigDto.java
+++ alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/api/dto/ContainerConfigDto.java
@@ -1,42 +1,10 @@
 package com.alinesno.infra.ops.container.api.dto;
 
+import lombok.Data;
+
+@Data
 public class ContainerConfigDto {
     private ContainerInfo info;
     private ContainerDetails container;
     private StorageConfig storage;
-
-    // getter and setter methods
-
-    public ContainerInfo getInfo() {
-        return info;
-    }
-
-    public void setInfo(ContainerInfo info) {
-        this.info = info;
-    }
-
-    public ContainerDetails getContainer() {
-        return container;
-    }
-
-    public void setContainer(ContainerDetails container) {
-        this.container = container;
-    }
-
-    public StorageConfig getStorage() {
-        return storage;
-    }
-
-    public void setStorage(StorageConfig storage) {
-        this.storage = storage;
-    }
-
-    @Override
-    public String toString() {
-        return "ContainerConfigDto{" +
-                "info=" + info +
-                ", container=" + container +
-                ", storage=" + storage +
-                '}';
-    }
 }
