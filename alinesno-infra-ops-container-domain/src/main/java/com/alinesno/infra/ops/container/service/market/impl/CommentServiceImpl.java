dcda55f578fcc02ff49b8d7f06bcfb7c050a284b
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:18 +0800

    更新:alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/service/market/impl/CommentServiceImpl.java

--- alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/service/market/impl/CommentServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.ops.container.service.market.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.ops.container.entity.market.CommentEntity;
-import com.alinesno.infra.ops.container.mapper.market.CommentMapper;
-import com.alinesno.infra.ops.container.service.market.ICommentService;
-
-public class CommentServiceImpl extends IBaseServiceImpl<CommentEntity , CommentMapper> implements ICommentService {
-}
