34f18f747c93d0cafb1d6689cdc4f803c56b4b77
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:18 +0800

    更新:alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/market/RatingMapper.java

--- alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/market/RatingMapper.java
+++
@@ -1,8 +0,0 @@
-package com.alinesno.infra.ops.container.mapper.market;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.ops.container.entity.market.CommentEntity;
-import com.alinesno.infra.ops.container.entity.market.RatingEntity;
-
-public interface RatingMapper extends IBaseMapper<RatingEntity> {
-}
