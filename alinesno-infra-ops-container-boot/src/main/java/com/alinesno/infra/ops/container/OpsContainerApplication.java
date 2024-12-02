9e1859d24bb1c0dee6520d74c93cf97525cea6b3
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    移除不必要的注解，优化应用启动类

--- alinesno-infra-ops-container-boot/src/main/java/com/alinesno/infra/ops/container/OpsContainerApplication.java
+++ alinesno-infra-ops-container-boot/src/main/java/com/alinesno/infra/ops/container/OpsContainerApplication.java
@@ -1,20 +1,14 @@
 package com.alinesno.infra.ops.container;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
  */
-@EnableActable
-@EnableInfraSsoApi
-@MapperScan("com.alinesno.infra.ops.container.mapper")
 @SpringBootApplication
 public class OpsContainerApplication {
 
