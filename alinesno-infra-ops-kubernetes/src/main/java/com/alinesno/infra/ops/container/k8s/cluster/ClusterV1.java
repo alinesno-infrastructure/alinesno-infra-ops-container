347e9acdb072688cced4f32e1ea932cb3fd2d0c1
李伟(Michael) <michaelAip@gmail.com>
2024-12-03 00:35:20 +0800

    feat:新增 ClusterV1 类，实现Kubernetes集群操作。

---
+++ alinesno-infra-ops-kubernetes/src/main/java/com/alinesno/infra/ops/container/k8s/cluster/ClusterV1.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.ops.container.k8s.cluster;
+
+import com.alinesno.infra.ops.container.k8s.KubernetesApiParent;
+
+/**
+ * Kubernetes集群操作类
+ */
+public class ClusterV1 extends KubernetesApiParent {
+
+
+
+}
