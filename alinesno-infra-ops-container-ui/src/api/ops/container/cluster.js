623201211599d9e860a409ac59ef91539c97e627
林晓(Lily) <lilyAip@gmail.com>
2024-12-04 00:29:39 +0800

    feat:添加集群和命名空间选择功能及相关API

--- alinesno-infra-ops-container-ui/src/api/ops/container/cluster.js
+++ alinesno-infra-ops-container-ui/src/api/ops/container/cluster.js
@@ -21,7 +21,34 @@ var managerUrl = {
     removeUrl: prefix + "delete" ,
     exportUrl: prefix + "exportExcel",
     changeField: prefix + "changeField",
-    downloadfile: prefix + "downloadfile"
+    downloadfile: prefix + "downloadfile",
+    currentCluster: prefix +"currentCluster",
+    choiceCluster: prefix +"choiceCluster",
+    choiceNamespace: prefix +"choiceNamespace",
+}
+
+// 配置当前空间
+export function choiceNamespace(namespace){
+    return request({
+        url: managerUrl.choiceNamespace + '?namespace=' + parseStrEmpty(namespace),
+        method: 'get'
+    })
+}
+
+// 配置当前应用
+export function choiceCluster(id){
+    return request({
+        url: managerUrl.choiceCluster + '?clusterId=' + parseStrEmpty(id),
+        method: 'get'
+    })
+}
+
+// 选择当前应用
+export function getCurrentCluster(){
+  return request({
+      url: managerUrl.currentCluster , 
+      method: 'get'
+  })
 }
 
 // 修改字段
