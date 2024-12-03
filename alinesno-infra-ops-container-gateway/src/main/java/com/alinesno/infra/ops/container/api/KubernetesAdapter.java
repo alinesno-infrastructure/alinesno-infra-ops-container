c0e8bd613c49f6cc77bdaae29f4c215ae8c45a96
王磊(Levi) <leviAip@gmail.com>
2024-12-04 00:29:39 +0800

    feat: 添加获取API客户端及命名空间的方法到KubernetesAdapter类。

--- alinesno-infra-ops-container-gateway/src/main/java/com/alinesno/infra/ops/container/api/KubernetesAdapter.java
+++ alinesno-infra-ops-container-gateway/src/main/java/com/alinesno/infra/ops/container/api/KubernetesAdapter.java
@@ -3,9 +3,12 @@ package com.alinesno.infra.ops.container.api;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.HttpStatus;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
+import com.alinesno.infra.ops.container.api.session.CurrentClusterSession;
+import com.alinesno.infra.ops.container.k8s.KubernetesApiClient;
 import com.alinesno.infra.ops.container.k8s.KubernetesPageBean;
 import com.alinesno.infra.ops.container.service.IClusterService;
 import io.kubernetes.client.openapi.ApiClient;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -21,14 +24,27 @@ public class KubernetesAdapter extends SuperController {
 	@Autowired
 	private IClusterService envClusterService;
 
+	@SneakyThrows
+	public ApiClient getApiClient() {
+		ClusterDto cluster = CurrentClusterSession.get() ;
+
+		if(cluster.getConfigType().equals("token")){
+			return KubernetesApiClient.getApiClientByToken(cluster.getApiServerUrl() , cluster.getKubeConfig()) ;
+		}else{
+			return KubernetesApiClient.getApiClientByConfig(cluster.getKubeConfig()) ;
+		}
+	}
+
 	/**
-	 * 根据用户获取配置的ApiClient
-	 * 
-	 * @param userId 用户ID
-	 * @return ApiClient对象
+	 * 获取当前集群的命名空间。
+	 * @return
 	 */
-	public ApiClient getApiClientByConfigByUserId(String userId) {
-		return envClusterService.getApiClientByConfigByUserId(userId);
+	public String getNamespace() {
+		String namespace =CurrentClusterSession.get().getNamespace();
+		if(namespace == null){
+			namespace = "default" ;
+		}
+		return namespace;
 	}
 
 	/**
