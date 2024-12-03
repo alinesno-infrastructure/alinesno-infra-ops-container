1bb02dc8de9b0156bf3bb72ca95c0790215dc95c
王磊(Levi) <leviAip@gmail.com>
2024-12-04 00:29:39 +0800

    feat:新增 ClusterUserMapper 接口，实现基本的事件管理映射功能。

---
+++ alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/ClusterUserMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.ops.container.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.ops.container.entity.ClusterUserEntity;
+
+/**
+ * 事件 Mapper接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface ClusterUserMapper extends IBaseMapper<ClusterUserEntity> {
+}
