389c7d4dbebfcb0f33069072c0ddcb698f2f2e9c
王磊(Levi) <leviAip@gmail.com>
2024-12-04 00:29:39 +0800

    优化 ClusterEntity 类，更新字段类型及注释

--- alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/entity/ClusterEntity.java
+++ alinesno-infra-ops-container-facade/src/main/java/com/alinesno/infra/ops/container/entity/ClusterEntity.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.ops.container.entity;
 
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
@@ -65,103 +66,8 @@ public class ClusterEntity extends InfraBaseEntity {
      * 集群配置
      */
     @TableField("kube_config")
-	@ColumnType(length=2048)
+	@ColumnType(MySqlTypeConstant.LONGTEXT)
 	@ColumnComment("集群配置")
     private String kubeConfig;
 }
 
-//    /**
-//     * 有效日期
-//     */
-//    @TableField("expiry_date")
-//	@ColumnType(length=255)
-//	@ColumnComment("有效日期")
-//    private String expiryDate;
-//
-//    /**
-//     * 节点数
-//     */
-//    @TableField("node_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("节点数")
-//    private String nodeCount;
-//
-//    /**
-//     * 物理服务器信息
-//     */
-//    @TableField("physical_server_info")
-//	@ColumnType(length=255)
-//	@ColumnComment("物理服务器信息")
-//    private String physicalServerInfo;
-//
-//    /**
-//     * 物理存储信息
-//     */
-//    @TableField("physical_storage_info")
-//	@ColumnType(length=255)
-//	@ColumnComment("物理存储信息")
-//    private String physicalStorageInfo;
-//
-//    /**
-//     * 物理内存信息
-//     */
-//    @TableField("physical_memory_info")
-//	@ColumnType(length=255)
-//	@ColumnComment("物理内存信息")
-//    private String physicalMemoryInfo;
-//
-//    /**
-//     * 密钥数
-//     */
-//    @TableField("key_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("密钥数")
-//    private String keyCount;
-//
-//    /**
-//     * 守护进程服务数
-//     */
-//    @TableField("daemon_service_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("守护进程服务数")
-//    private String daemonServiceCount;
-//
-//    /**
-//     * 无状态服务数
-//     */
-//    @TableField("stateless_service_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("无状态服务数")
-//    private String statelessServiceCount;
-//
-//    /**
-//     * 存储卷数
-//     */
-//    @TableField("volume_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("存储卷数")
-//    private String volumeCount;
-//
-//    /**
-//     * 服务数
-//     */
-//    @TableField("service_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("服务数")
-//    private String serviceCount;
-//
-//    /**
-//     * 容器组数量
-//     */
-//    @TableField("container_group_count")
-//	@ColumnType(length=255)
-//	@ColumnComment("容器组数量")
-//    private String containerGroupCount;
-//
-//    /**
-//     * 状态
-//     */
-//    @TableField("status")
-//	@ColumnType(length=255)
-//	@ColumnComment("状态")
-//    private String status;
