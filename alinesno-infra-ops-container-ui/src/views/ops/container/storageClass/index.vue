3cbc2531541732ec3ed0b19e51a044bbe37ded59
王明(AllenQin) <allenQin@gmail.com>
2024-12-05 00:32:09 +0800

    fix: 更新存储类页面表单字段名称及提示信息

--- alinesno-infra-ops-container-ui/src/views/ops/container/storageClass/index.vue
+++ alinesno-infra-ops-container-ui/src/views/ops/container/storageClass/index.vue
@@ -1,14 +1,14 @@
 <template>
    <div class="app-container">
       <el-row :gutter="20">
-         <!--应用数据-->
+         <!-- 持久卷声明数据 -->
          <el-col :span="24" :xs="24">
             <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="100px">
-               <el-form-item label="应用名称" prop="dbName">
-                  <el-input v-model="queryParams.dbName" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+               <el-form-item label="PVC名称" prop="name">
+                  <el-input v-model="queryParams.name" placeholder="请输入PVC名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
                </el-form-item>
-               <el-form-item label="应用名称" prop="dbName">
-                  <el-input v-model="queryParams['condition[dbName|like]']" placeholder="请输入应用名称" clearable style="width: 240px" @keyup.enter="handleQuery" />
+               <el-form-item label="命名空间" prop="namespace">
+                  <el-input v-model="queryParams.namespace" placeholder="请输入命名空间" clearable style="width: 240px" @keyup.enter="handleQuery" />
                </el-form-item>
                <el-form-item>
                   <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -17,7 +17,6 @@
             </el-form>
 
             <el-row :gutter="10" class="mb8">
-
                <el-col :span="1.5">
                   <el-button type="primary" plain icon="Plus" @click="handleAdd">新增</el-button>
                </el-col>
@@ -27,87 +26,87 @@
                <el-col :span="1.5">
                   <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete">删除</el-button>
                </el-col>
-
                <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
             </el-row>
 
-            <el-table v-loading="loading" :data="ApplicationList" @selection-change="handleSelectionChange">
+            <el-table v-loading="loading" :data="storageClassList" @selection-change="handleSelectionChange">
                <el-table-column type="selection" width="50" align="center" />
-               <el-table-column label="图标" align="center" with="80" key="status" v-if="columns[5].visible">
-               </el-table-column>
-
-               <!-- 业务字段-->
-               <el-table-column label="应用名称" align="center" key="dbName" prop="dbName" v-if="columns[0].visible" />
-               <el-table-column label="应用描述" align="center" key="dbDesc" prop="dbDesc" v-if="columns[1].visible" :show-overflow-tooltip="true" />
-               <el-table-column label="表数据量" align="center" key="nickName" prop="nickName" v-if="columns[2].visible" :show-overflow-tooltip="true" />
-               <el-table-column label="类型" align="center" key="dbType" prop="dbType" v-if="columns[3].visible" :show-overflow-tooltip="true" />
-               <el-table-column label="应用地址" align="center" key="jdbcUrl" prop="jdbcUrl" v-if="columns[4].visible" width="120" />
-               <el-table-column label="状态" align="center" key="hasStatus" v-if="columns[5].visible" />
-
-               <el-table-column label="添加时间" align="center" prop="addTime" v-if="columns[6].visible" width="160">
+               
+               <!-- 业务字段 -->
+               <el-table-column label="名称" align="center" key="name" prop="name" v-if="columns[0].visible" />
+               <el-table-column label="命名空间" align="center" key="namespace" prop="namespace" v-if="columns[1].visible" />
+               <el-table-column label="状态" align="center" key="status" prop="status" v-if="columns[2].visible" />
+               <el-table-column label="关联的PV名称" align="center" key="volumeName" prop="volumeName" v-if="columns[3].visible" />
+               <el-table-column label="请求容量" align="center" key="capacity" prop="capacity" v-if="columns[4].visible" />
+               <el-table-column label="访问模式" align="center" key="accessModes" prop="accessModes" v-if="columns[5].visible" />
+               <el-table-column label="创建时间" align="center" prop="creationTimestamp" v-if="columns[6].visible" width="160">
                   <template #default="scope">
-                     <span>{{ parseTime(scope.row.addTime) }}</span>
+                     <span>{{ parseTime(scope.row.creationTimestamp) }}</span>
                   </template>
                </el-table-column>
+               <el-table-column label="存活时间" align="center" prop="age" v-if="columns[1].visible" />
 
-               <!-- 操作字段  -->
+               <!-- 操作字段 -->
                <el-table-column label="操作" align="center" width="150" class-name="small-padding fixed-width">
                   <template #default="scope">
-                     <el-tooltip content="修改" placement="top" v-if="scope.row.ApplicationId !== 1">
+                     <el-tooltip content="修改" placement="top" v-if="scope.row.PVCId !== 1">
                         <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
-                           v-hasPermi="['system:Application:edit']"></el-button>
+                           v-hasPermi="['system:PVC:edit']">修改</el-button>
                      </el-tooltip>
-                     <el-tooltip content="删除" placement="top" v-if="scope.row.ApplicationId !== 1">
+                     <el-tooltip content="删除" placement="top" v-if="scope.row.PVCId !== 1">
                         <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
-                           v-hasPermi="['system:Application:remove']"></el-button>
+                           v-hasPermi="['system:PVC:remove']">删除</el-button>
                      </el-tooltip>
                   </template>
-
                </el-table-column>
             </el-table>
             <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize" @pagination="getList" />
          </el-col>
       </el-row>
 
-      <!-- 添加或修改应用配置对话框 -->
+      <!-- 添加或修改持久卷声明配置对话框 -->
       <el-dialog :title="title" v-model="open" width="900px" append-to-body>
          <el-form :model="form" :rules="rules" ref="databaseRef" label-width="100px">
             <el-row>
                <el-col :span="24">
-                  <el-form-item label="名称" prop="dbName">
-                     <el-input v-model="form.dbName" placeholder="请输入应用名称" maxlength="50" />
+                  <el-form-item label="名称" prop="name">
+                     <el-input v-model="form.name" placeholder="请输入PVC名称" maxlength="50" />
                   </el-form-item>
                </el-col>
             </el-row>
             <el-row>
                <el-col :span="24">
-                  <el-form-item label="连接" prop="jdbcUrl">
-                     <el-input v-model="form.jdbcUrl" placeholder="请输入jdbcUrl连接地址" maxlength="128" />
+                  <el-form-item label="命名空间" prop="namespace">
+                     <el-input v-model="form.namespace" placeholder="请输入命名空间" maxlength="50" />
                   </el-form-item>
                </el-col>
                <el-col :span="24">
-                  <el-form-item label="类型" prop="dbType">
-                     <el-input v-model="form.dbType" placeholder="请输入类型" maxlength="50" />
+                  <el-form-item label="状态" prop="status">
+                     <el-input v-model="form.status" placeholder="请输入状态" maxlength="50" />
                   </el-form-item>
                </el-col>
             </el-row>
             <el-row>
                <el-col :span="24">
-                  <el-form-item label="用户名" prop="dbUsername">
-                     <el-input v-model="form.dbUsername" placeholder="请输入连接用户名" maxlength="30" />
+                  <el-form-item label="关联的PV名称" prop="volumeName">
+                     <el-input v-model="form.volumeName" placeholder="请输入关联的PV名称" maxlength="50" />
                   </el-form-item>
                </el-col>
                <el-col :span="24">
-                  <el-form-item label="密码" prop="dbPasswd">
-                     <el-input v-model="form.dbPasswd" placeholder="请输入应用密码" type="password" maxlength="30" show-password />
+                  <el-form-item label="请求容量" prop="capacity">
+                     <el-input v-model="form.capacity" placeholder="请输入请求容量" maxlength="50" />
                   </el-form-item>
                </el-col>
             </el-row>
-
             <el-row>
                <el-col :span="24">
-                  <el-form-item label="备注" prop="dbDesc">
-                     <el-input v-model="form.dbDesc" placeholder="请输入应用备注"></el-input>
+                  <el-form-item label="访问模式" prop="accessModes">
+                     <el-input v-model="form.accessModes" placeholder="请输入访问模式" maxlength="50" />
+                  </el-form-item>
+               </el-col>
+               <el-col :span="24">
+                  <el-form-item label="标签" prop="labels">
+                     <el-input v-model="form.labels" placeholder="请输入标签"></el-input>
                   </el-form-item>
                </el-col>
             </el-row>
@@ -119,25 +118,24 @@
             </div>
          </template>
       </el-dialog>
-
    </div>
 </template>
 
-<script setup name="Application">
+<script setup name="StorageClass">
 
 import {
-   listApplication,
-   delApplication,
-   getApplication,
-   updateApplication,
-   addApplication
-} from "@/api/ops/container/plugins";
+   listStorageClass,
+   delStorageClass,
+   getStorageClass,
+   updateStorageClass,
+   addStorageClass
+} from "@/api/ops/container/storageClass";
 
 const router = useRouter();
 const { proxy } = getCurrentInstance();
 
 // 定义变量
-const ApplicationList = ref([]);
+const storageClassList = ref([]);
 const open = ref(false);
 const loading = ref(true);
 const showSearch = ref(true);
@@ -184,9 +182,9 @@ const { queryParams, form, rules } = toRefs(data);
 /** 查询应用列表 */
 function getList() {
    loading.value = true;
-   listApplication(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
+   listStorageClass(proxy.addDateRange(queryParams.value, dateRange.value)).then(res => {
       loading.value = false;
-      ApplicationList.value = res.rows;
+      storageClassList.value = res.rows;
       total.value = res.total;
    });
 };
@@ -207,9 +205,9 @@ function resetQuery() {
 };
 /** 删除按钮操作 */
 function handleDelete(row) {
-   const ApplicationIds = row.id || ids.value;
-   proxy.$modal.confirm('是否确认删除应用编号为"' + ApplicationIds + '"的数据项？').then(function () {
-      return delApplication(ApplicationIds);
+   const StorageClassIds = row.id || ids.value;
+   proxy.$modal.confirm('是否确认删除应用编号为"' + StorageClassIds + '"的数据项？').then(function () {
+      return delStorageClass(StorageClassIds);
    }).then(() => {
       getList();
       proxy.$modal.msgSuccess("删除成功");
@@ -228,7 +226,7 @@ function reset() {
    form.value = {
       id: undefined,
       deptId: undefined,
-      ApplicationName: undefined,
+      StorageClassName: undefined,
       nickName: undefined,
       password: undefined,
       phonenumber: undefined,
@@ -253,8 +251,8 @@ function handleAdd() {
 /** 修改按钮操作 */
 function handleUpdate(row) {
    reset();
-   const ApplicationId = row.id || ids.value;
-   getApplication(ApplicationId).then(response => {
+   const StorageClassId = row.id || ids.value;
+   getStorageClass(StorageClassId).then(response => {
       form.value = response.data;
       open.value = true;
       title.value = "修改应用";
@@ -265,14 +263,14 @@ function handleUpdate(row) {
 function submitForm() {
    proxy.$refs["databaseRef"].validate(valid => {
       if (valid) {
-         if (form.value.ApplicationId != undefined) {
-            updateApplication(form.value).then(response => {
+         if (form.value.StorageClassId != undefined) {
+            updateStorageClass(form.value).then(response => {
                proxy.$modal.msgSuccess("修改成功");
                open.value = false;
                getList();
             });
          } else {
-            addApplication(form.value).then(response => {
+            addStorageClass(form.value).then(response => {
                proxy.$modal.msgSuccess("新增成功");
                open.value = false;
                getList();
