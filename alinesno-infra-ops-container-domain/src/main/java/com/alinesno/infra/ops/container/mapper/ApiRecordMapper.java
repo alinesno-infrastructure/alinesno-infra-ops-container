41c5f4cbf9a0ebd729b49bf4f624b1cfb5ac3d45
陈伟(Victor) <victorAip@gmail.com>
2024-12-05 00:32:09 +0800

    feat:新增 ApiRecordMapper 接口，实现数据持久层操作。

---
+++ alinesno-infra-ops-container-domain/src/main/java/com/alinesno/infra/ops/container/mapper/ApiRecordMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.ops.container.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.ops.container.entity.ApiRecordEntity;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface ApiRecordMapper extends IBaseMapper<ApiRecordEntity> {
+
+}
